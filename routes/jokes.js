f859b6eabaa515225927b9a5ecebb4f5adab9c0f
Brian <40767783+brishaw@users.noreply.github.com>
2019-02-12 16:21:42 -0500

    added 1 test route, 2 api calls and 2 scrapers...

--- routes/jokes.js
+++ routes/jokes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const axios = require('axios');
+const cheerio = require('cheerio');
 const jokeController = express.Router();
 
 console.log('joke controller is running...');
@@ -10,6 +11,94 @@ jokeController.route('/test')
     res.json({
       message: 'SUCCESS TO ROUTE!'
     });
+  })
+
+  jokeController.route('/dadjoke')
+    .get((req, res) => {
+      axios.get('https://icanhazdadjoke.com/', { 
+        headers: {
+        'Accept': 'application/json'
+        }
+      })
+        .then((response) => {
+          console.log(response.data.joke);
+          res.json({
+            joke: response.data.joke
+          }); 
+        })
+        .catch((error) => {
+          console.log('Something is wrong with the GET request...');
+          console.log(error);
+        });
+    })
+
+jokeController.route('/chuckjoke')
+  .get((req, res) => {
+    axios.get('http://api.icndb.com/jokes/random')
+      .then((response) => {
+        console.log(response.data.value.joke);
+        res.json({
+          joke: response.data.value.joke
+        });
+      })
+      .catch((error) => {
+        console.log('Something is wrong with the GET request...');
+        console.log(error);
+      })
+  })
+
+  jokeController.route('/funnyjoke')
+    .get((req, res) => {
+      let i = Math.floor((Math.random() * 35) + 1);
+      axios.get('http://www.laughfactory.com/jokes/clean-jokes/' + i + '/')
+        .then((response) => {
+          var $ = cheerio.load(response.data);
+          var results = [];
+          $("div.joke-text p").each(function (i, element) {
+            var title =$(element).text();
+
+            results.push({
+              title: title
+            });
+          })
+          console.log(results);
+          //return results;
+          res.json({
+            jokes: results
+          })
+
+        })
+       .catch ((error) => {
+        console.log('Something is wrong with the GET request...');
+        console.log(error);
+      })
+    })
+
+jokeController.route('/riddle')
+  .get((req, res) => {
+    let i = Math.floor((Math.random() * 47) + 1);
+    axios.get('https://goodriddlesnow.com/riddles/by/funny-riddles/page:' + i + '/')
+      .then((response) => {
+        var $ = cheerio.load(response.data);
+        var results = [];
+        $("div.riddle-question p, div.riddle-answer p").each(function (i, element) {
+          var title = $(element).text();
+
+          results.push({
+            title: title
+          });
+        })
+        console.log(results);
+        //return results;
+        res.json({
+          jokes: results
+        })
+
+      })
+      .catch((error) => {
+        console.log('Something is wrong with the GET request...');
+        console.log(error);
+      })
   })
 
   module.exports = jokeController;
