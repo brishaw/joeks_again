f170db5aac425413a24c00a1d2a529c429377893
Brian <40767783+brishaw@users.noreply.github.com>
2019-02-15 05:00:49 -0500

    edited server.js...

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-//const path = require('path');
+const path = require('path');
 
 const jokeController = require('./routes/jokes');
 
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static("app/public"));
 
 app.use('/api', jokeController);
 
